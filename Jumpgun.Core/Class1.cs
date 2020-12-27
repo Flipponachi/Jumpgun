e4a6a07feea0f51372e47adc632576ee51426572
Dayo Ojo Jnr <dayo@kudabank.com>
2020-12-27 23:13:24 +0100

    Initial commit

---
+++ Jumpgun.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Jumpgun.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ Jumpgun.Tests/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Jumpgun.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+    }
+}
